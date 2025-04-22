60cfbe6823822976230d5b87d4b31d134dae0c1a
Krishna <saikrishnakasimalla72@gmail.com>
2025-04-22 14:56:16 +0530

    first commit

--- src/main/java/com/example/SimpleStudentManagement/SimpleStudentManagementApplication.java
+++ src/main/java/com/example/SimpleStudentManagement/SimpleStudentManagementApplication.java
@@ -2,12 +2,10 @@ package com.example.SimpleStudentManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class SimpleStudentManagementApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleStudentManagementApplication.class, args);
 	}
-
 }
+
